3f752d9d8374701e46922ba40a884d8966297956
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-08-19 17:17:33 +0900

    HTTP API 메소드 명시

--- src/main/java/com/controller/BoardController.java
+++ src/main/java/com/controller/BoardController.java
@@ -9,7 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,9 +20,8 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
-    @RequestMapping("/board/openBoardList.do")
+    @RequestMapping(value = "/board/openBoardList.do",method = RequestMethod.GET)
     public ModelAndView openBoardList() throws Exception {
-        int i = 10 / 0;
         ModelAndView mv = new ModelAndView();
         mv.setViewName("boardList");
         List<BoardDto> list = boardService.selectBoardList();
@@ -29,18 +30,18 @@ public class BoardController {
         return mv;
     }
 
-    @RequestMapping("/board/openBoardWrite.do")
+    @RequestMapping(value = "/board/openBoardWrite.do", method = RequestMethod.GET)
     public String openBoardWrite() throws Exception {
         return "boardWrite";
     }
 
-    @RequestMapping("/board/insertBoard.do")
-    public String insertBoard(BoardDto board) throws Exception {
-        boardService.insertBoard(board);
+    @RequestMapping(value = "/board/insertBoard.do", method = RequestMethod.POST)
+    public String insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception {
+        boardService.insertBoard(board, multipartHttpServletRequest);
         return "redirect:/board/openBoardList.do";
     }
 
-    @RequestMapping("/board/openBoardDetail.do")
+    @RequestMapping(value = "/board/openBoardDetail.do", method = RequestMethod.GET)
     public ModelAndView openBoardDetail(@RequestParam int boardIdx) throws Exception {
         ModelAndView mv = new ModelAndView("boardDetail");
         BoardDto board = boardService.selectBoardDetail(boardIdx);
@@ -49,13 +50,13 @@ public class BoardController {
         return mv;
     }
 
-    @RequestMapping("/board/updateBoard.do")
+    @RequestMapping(value = "/board/updateBoard.do", method = RequestMethod.PUT)
     public String updateBoard(BoardDto board) throws Exception {
         boardService.updateBoard(board);
         return "redirect:/board/openBoardList.do";
     }
 
-    @RequestMapping("/board/deleteBoard.do")
+    @RequestMapping(value = "/board/deleteBoard.do", method = RequestMethod.DELETE)
     public String deleteBoard(int boardIdx) throws Exception {
         boardService.deleteBoard(boardIdx);
         return "redirect:/board/openBoardList.do";
