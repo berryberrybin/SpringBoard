5d37b377ad5c1a9d405ce622592c07311571864c
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-09-17 20:48:09 +0900

    DB 쿼리 결과를 받아오기 위한 Param DTO 추가

---
+++ src/main/java/com/board/dto/Param.java
@@ -0,0 +1,31 @@
+package com.board.dto;
+
+public class Param {
+    private String title;
+    private String contents;
+    private String writer;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+}
